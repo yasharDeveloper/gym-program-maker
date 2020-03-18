93d5b5deb5973b11038073db8b0a109db2e57bf4
Yashar <yasharfoolagar2014@gmail.com>
2020-03-18 16:52:33 +0330

    create gym-layout.blade.php layout

---
+++ resources/views/layouts/gym-layout.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-rtl.min.css">
+    <script src="../node_modules/jquery/dist/jquery.min.js"></script>
+    <link rel="stylesheet" href="@yield('styles')">
+    <script src="@yield('js-codes')"></script>
+
+    <title>@yield('title')</title>
+</head>
+<body>
+    @section('sidebar')
+        <h3>this is a simple sidebar</h3>
+
+    @show
+
+    <div class="container">
+        @yield('content')
+    </div>
+
+
+</body>
+</html>
