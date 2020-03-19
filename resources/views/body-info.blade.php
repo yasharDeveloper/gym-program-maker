0c7d374de85c4cf9470c6c95f76b9d6b743e73a6
Yashar <yasharfoolagar2014@gmail.com>
2020-03-19 16:46:18 +0330

    modify body-info.blade.php

--- resources/views/body-info.blade.php
+++ resources/views/body-info.blade.php
@@ -12,8 +12,29 @@
 
 
 @section('content')
-    <div class="info-wrapper">
-        
+    <div class="container">
+        <div class="row justify-content-center">
+
+            <div class="col-6 info-wrapper">
+
+                <div class="row justify-content-center info-wrapper-header">
+                    <h3 >اطلاعات</h3>
+                </div>
+
+                <div class="row">
+                    <h3 class="col-2">وزن</h3>
+                    <h3 class="col-2">70</h3>
+                </div>
+
+                <hr>
+
+                <div class="row">
+                    <h3 class="col-2">وزن</h3>
+                    <h3 class="col-2">70</h3>
+                </div>
+
+            </div>
+        </div>
     </div>
 
 @endsection
